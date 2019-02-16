bdb522d2c9ea985e37824f2db60116ddb5f8c737
Gustavo Arellano <gustavo813arellano@gmail.com>
2019-02-15 16:22:30 -0800

    fixed template and static linking issues

--- apps/main_app/views.py
+++ apps/main_app/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.http import HttpResponseRedirect
 
 def index(request):
-    return render(request, "main_app/index.html")
+    return render(request, "index.html")
 
 
